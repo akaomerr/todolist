fcef1a97a8208370b8f3d5fac406c67000e80ae1
akabetta <omerr_ylmz@hotmail.com>
2023-09-27 16:24:58 +0300

    site recreated

--- save.js
+++ save.js
@@ -1,33 +1,73 @@
-document.addEventListener("DOMContentLoaded",function(){
-    const items=document.getElementById("items");
-    const saveButton=document.getElementById("save");
-    const notList=document.getElementById("notList");
-    const oldnotes=JSON.parse(localStorage.getItem("userNotes"))||[];
-    function printNotes(){
-        notList.innerHTML="";
-        for (const note of oldnotes){
-            const notElement=document.createElement("li");
-            notElement.textContent=note;
-            const delButton=document.createElement("button");
-            delButton.textContent="Delete";
-            delButton.addEventListener("click",function(){
-                const delIndex=oldnotes.indexOf(note);
-                oldnotes.splice(delIndex,1);
-                localStorage.setItem("userNotes",JSON.stringify(oldnotes));
-                printNotes();
-            });
-            notElement.appendChild(delButton);
-            notList.appendChild(notElement);
-        }
-    }
-    printNotes();
-    saveButton.addEventListener("click",function(){
-        const newNot=items.value;
-        if(newNot){
-            oldnotes.push(newNot);
-            localStorage.setItem("userNotes",JSON.stringify(oldnotes));
-            printNotes();
-            items.value="";
+window.addEventListener('load',function(){
+    displaySavedItems();
+});
+document.getElementById("delete").addEventListener("click",function(){
+    var checkboxes=document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox){
+        if(checkbox.checked){
+            checkbox.parentElement.remove();
+            savedItemsLocalStorage();
         }
     });
 });
+document.getElementById("items").addEventListener("keyup", function(event){
+    if(event.key==="Enter"){
+        var newItem=document.getElementById("items").value;
+        if(newItem.trim() !==""){
+            addItemToList(newItem);
+            document.getElementById("items").value = ""
+            savedItemsLocalStorage();
+        }
+    }
+});
+
+document.getElementById("save").addEventListener("click",function(){
+    var newItem=document.getElementById("items").value;
+    if(newItem.trim() !==""){
+        addItemToList(newItem);
+        document.getElementById("items").value = ""
+        savedItemsLocalStorage();
+    }
+});
+
+function addItemToList(text){
+    var noteList=document.getElementById("noteList");
+    var listItem=document.createElement("li");
+    listItem.className="list-group-item";
+
+    var checkbox=document.createElement("input");
+    checkbox.className="form-check-input me-sm-3";
+    checkbox.type="checkbox";
+
+    var label = document.createElement("label");
+    label.className = "form-check-label label-wrap";
+    label.textContent = text;
+    
+    listItem.appendChild(checkbox);
+    listItem.appendChild(label);
+    noteList.appendChild(listItem);
+}
+
+function savedItemsLocalStorage(){
+    var items=[];
+    var checkboxes=document.querySelectorAll('input[type="checkbox"]');
+    checkboxes.forEach(function(checkbox){
+        var label=checkbox.nextElementSibling;
+        items.push({text:label.textContent,checked:checkbox.checked});
+    });
+    localStorage.setItem("todoItems",JSON.stringify(items));
+}
+
+function displaySavedItems() {
+    var savedItems = localStorage.getItem("todoItems");
+    if (savedItems) {
+        savedItems = JSON.parse(savedItems);
+        savedItems.forEach(function (item) {
+            addItemToList(item.text);
+            if (item.checked) {
+                var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+                checkboxes[checkboxes.length - 1].checked = true;
+            }
+        });
+    }
+}
